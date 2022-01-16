f6a97196e6ff498d042f519818b04cb2e1b47056
Gal Ben-Shushan <76122096+galbenshushan@users.noreply.github.com>
2022-01-16 03:03:41 +0200

    2

--- front/src/helpers/localStorage.js
+++
@@ -1,5 +0,0 @@
-export const setItemToLocalStorage = (storageName, arr) =>{ localStorage.setItem(storageName, JSON.stringify(arr));
-}
-export const getItemFromLocalStorage = (storageName) => JSON.parse(localStorage.getItem(storageName));
-
-export const deleteFromLocalStorage = (storageName) => localStorage.removeItem(`${storageName}`);
