97b52ffeacf7cfdd8a0d89d237b562543d992057
Gal Ben-Shushan <galbenshushan5@gmail.com>
2022-10-30 20:29:03 +0200

    fixes before refactor

--- front/src/components/Pokemon/CardDetails/StatsCollapse.js
+++ front/src/components/Pokemon/CardDetails/StatsCollapse.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { Button, CardContent, Collapse, Typography } from '@mui/material';
+import React from "react";
+import { Button, CardContent, Collapse, Typography } from "@mui/material";
 
-const StatsCollapse = ({expanded, pokemon, dynamicText}) => {
-    return <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-            <Typography style={dynamicText} paragraph>
-                STATS:
-            </Typography>
-            <Typography style={dynamicText} paragraph>
-                {pokemon.stats.map((stat, idx) =>
-                    <Button className='disbtn' variant="outlined" key={idx} style={dynamicText}>{stat.stat.name}: {stat.base_stat} </Button>
-                )}
-            </Typography>
-        </CardContent>
-    </Collapse>;
+const StatsCollapse = ({ expanded, pokemon, dynamicText }) => {
+  return (
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <CardContent>
+        <Typography style={dynamicText} paragraph>
+          STATS:
+        </Typography>
+        <Typography style={dynamicText} paragraph>
+          {pokemon.stats.map((stat, idx) => (
+            <Button
+              className="disbtn"
+              variant="outlined"
+              key={idx}
+              style={dynamicText}
+            >
+              {stat.stat.name}: {stat.base_stat}{" "}
+            </Button>
+          ))}
+        </Typography>
+      </CardContent>
+    </Collapse>
+  );
 };
 
 export default StatsCollapse;
